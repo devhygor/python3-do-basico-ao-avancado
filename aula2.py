ea102447b118c851e55c0dd0fee0da0392782628
Hygor Rocha <hygor.k92@gmail.com>
2023-01-15 23:45:27 -0300

    um pouco mais sobre print

---
+++ aula2.py
@@ -0,0 +1,4 @@
+# mostrando que na funçao print podemos passar varios argumentos.
+print(12, 34, sep='-', end='#')
+print(34, 56, sep='-', end='\n')
+print(9, 10, sep='-', end='\n')
---
+++ aula3.py
@@ -0,0 +1,21 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+String são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Hygor Rocha')
+print(1, 'Hygor "Rocha"')
+
+# Aspas duplas
+print("Hygor Rocha")
+print(2, "Hygor 'Rocha'")
+
+# Escape
+print("Hygor \"Rocha\"")
+
+# r
+print(r"Hygor \"Rocha\"")
