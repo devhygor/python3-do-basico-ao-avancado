94d2edf926d0e0a734bbe962222b01bf351226b0
Hygor Rocha <hygor.k92@gmail.com>
2024-08-24 23:20:47 -0300

    add aula 12, calculadora imc

---
+++ aula12.py
@@ -0,0 +1,6 @@
+nome = 'Hygor Rocha'
+altura = 1.83
+peso = 86.90
+imc = peso / (altura * altura) # IMC =  peso / (altura x altura)
+
+print(nome, ' tem ', altura, 'pesa ', peso, 'quilos e seu IMC é: ', imc)
