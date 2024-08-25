511bd57299900dc34632713d3ff35e299692917b
Hygor Rocha <hygor.k92@gmail.com>
2024-08-24 22:35:50 -0300

    add aula 6

---
+++ aula6.py
@@ -0,0 +1,10 @@
+# conversão de tipos, coerção
+# type convertion, typecasting, coercion
+# é o ato de converter um tipo em outro
+# tipos imutáveis e primitivos:
+# str, int, float, bool
+print(1 + 1)
+print('a' + 'b')
+# print('1' + 1) da erro pq nao é possivel concatenar uma str com um int
+print(int('1'), type(int('1')))
+print(int('1') + 1)
