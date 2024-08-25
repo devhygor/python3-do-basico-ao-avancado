a70e606ed6ad53ae154ff4f6e3adea3638437e4f
Hygor Rocha <hygor.k92@gmail.com>
2024-08-24 22:46:41 -0300

    add aula 7

---
+++ aula7.py
@@ -0,0 +1,15 @@
+# Váriaveis são usadas para salvar algo na memória do computador.
+# PEP8: inicia variáveis com letras minúsculas, pode usar
+# números e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variavel).
+# Uso: nome variavel =  expressao
+nome_completo = 'Hygor Melo Rocha'
+soma_dois_mais_dois = 2 + 2
+print(nome_completo, soma_dois_mais_dois)
+
+nome = 'Hygor'
+idade = 27
+maior_de_idade = idade >= 10
+print('Nome', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
