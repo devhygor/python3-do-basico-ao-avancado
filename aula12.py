75b46acbdcb871b27339970863a61debedca42b5
Hygor Rocha <hygor.k92@gmail.com>
2024-08-24 23:22:11 -0300

    add aula 12, calculadora imc

--- aula12.py
+++ aula12.py
@@ -3,4 +3,4 @@ altura = 1.83
 peso = 86.90
 imc = peso / (altura * altura) # IMC =  peso / (altura x altura)
 
-print(nome, ' tem ', altura, 'pesa ', peso, 'quilos e seu IMC é: ', imc)
+print(nome, 'tem', altura, 'pesa', peso, 'quilos e seu IMC é:', imc)
