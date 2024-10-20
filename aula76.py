e464c10e111397bc89e043d199ddbd90933f3e34
Hygor Rocha <hygor.k92@gmail.com>
2024-10-20 08:27:48 -0300

    add aula 76, exercicio acertar a palavra

--- aula76.py
+++ aula76.py
@@ -11,38 +11,44 @@ na palavra secreta.
     palavra secreta; exiba a letra;
     - Se a letra digitada não estiver
     na palavra secreta; exiba *.
-Faça a contagem de tentativas do seu 
+Faça a contagem de numero_tentativas do seu 
 usuário.
 """
+import os
+
 
 palavra_secreta = input('QUAL SERA A PALAVRA SECRETA? ').upper()
-palavra_corrigida = ''
+os.system('clear')
+letras_acertadas = ''
+numero_tentativas = 0
 
-tentativas = 0
-
-# while len(palavra_corrigida) != len(palavra_secreta):
 while True:
-    tentativas += 1
-    print(f'Tentativa n° {tentativas}')
-    letra = input('DIGITE UMA LETRA: ').upper()
+    palavra_corrigida = ''
+    numero_tentativas += 1
+    print(f'Tentativa n° {numero_tentativas}')
+    letra_digitada = input('DIGITE UMA LETRA: ').upper()
     
-    if len(letra) != 1:
+    if len(letra_digitada) != 1:
         print('DIGITE APENAS UMA LETRA!')
         continue
+    
+    if letra_digitada in palavra_secreta:
+        letras_acertadas += letra_digitada
 
-    if letra in palavra_secreta:
-        palavra_corrigida += letra
-        if len(palavra_corrigida) == 1:
-            print(f'Letra correta: "{letra}"\n')
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letras_acertadas:
+            palavra_corrigida += letra_secreta
         else:
-            print(f'Letras corretas: "{palavra_corrigida}"\n')
-        
-        for letra_secreta in palavra_secreta:
-            if letra_secreta in palavra_secreta:
-                print(letra_secreta)
-    else:
-        print(f'A Letra "{letra}", nao existe na palavra secreta!\n')
-else:
-    print(f'Todas as letras da palavra secreta foram digitadas! {palavra_corrigida}')
-    print(f'A Palavra secreta era {palavra_secreta}')
+            palavra_corrigida += '*'
 
+    print(f'Palavra formada: {palavra_corrigida}\n')
+    
+    if palavra_corrigida == palavra_secreta:
+        os.system('clear')
+        print('VOCÊ GANHOU!! PARABÉNS!')
+        print(f'A palavra era "{palavra_secreta}"')
+        print(f'Total de tentativas: {numero_tentativas}\n')
+
+        palavra_secreta = input('QUAL SERA A PALAVRA SECRETA? ').upper()
+        letras_acertadas = ''
+        numero_tentativas = 0
